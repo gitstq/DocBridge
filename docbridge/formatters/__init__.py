03cd68e1547c91a693a2268cf37fc75d71abfe40
琦琦 <56579081+gitstq@users.noreply.github.com>
2026-05-17 10:21:57 +0800

    feat: add docbridge/formatters/__init__.py

---
+++ docbridge/formatters/__init__.py
@@ -0,0 +1,8 @@
+"""
+Formatters for DocBridge
+"""
+from .code import CodeFormatter
+from .table import TableFormatter
+from .ai_dialog import AIDialogFormatter
+
+__all__ = ["CodeFormatter", "TableFormatter", "AIDialogFormatter"]
